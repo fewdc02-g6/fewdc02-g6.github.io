f7306fd79d0ac23f4cbfbf63e66fc74674278f1f
anson <2516529973@qq.com>
2021-08-20 12:22:38 +0800

    addHover&Css (index.html,loginPage,creatAc.html)

--- Sprint-2/server_csv/public/main.js
+++ Sprint-2/server_csv/public/main.js
@@ -1,11 +1,53 @@
-fetch('http://localhost:8080/creatAc')
-.then(res => res.json()
-)
-.then((data)=>{
-    console.log(data)
-})
-.catch(err=> console.log('fale! '+err))
+// http://localhost:8080/user/register
 
+// async function getResult(){
+//     const res = await fetch('http://localhost:8080/user/register')
+//     .then((res)=>{
+//         console.log('success')
+//     })
+//     const data = await res.json()
+//     console.log(data)
+//     return data; 
+// }
+
+// getResult()
+
+
+
+
+
+
+async function showData() {    
+	const response = await fetch('http://localhost:8080/user/register')	
+	if (response.ok) {
+		const dataArr = await response.json()
+		return dataArr
+	}
+}
+showData()
+.then(res=>{
+    console.log('ok')
+    console.log(res)
+})
+.catch(err=>console.log('fale'))
+
+
+
+
+
+
+
+
+
+
+
+// fetch('http://localhost:8080/user/login')
+// .then(res => res.json()
+// )
+// .then((data)=>{
+//     console.log(data)
+// })
+// .catch(err=> console.log('fale! '+err))
 
 
 // fetch('http://localhost:8080/creatAc.html')
