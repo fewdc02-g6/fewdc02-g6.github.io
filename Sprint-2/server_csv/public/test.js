92a8ab5f3a27278c393949d8253d15938ca43a06
Skylar2021 <maplewong5@yahoo.com.hk>
2021-08-22 17:15:17 +0800

    update test.js

--- Sprint-2/server_csv/public/test.js
+++ Sprint-2/server_csv/public/test.js
@@ -24,10 +24,13 @@ window.addEventListener('load', () => {
 			getTaskByDate(date)
 		})
 	})
+	displayTaskArea.innerText ='Please Pick a Date.'
 })
 
-leftArrow.addEventListener('click', ()=>{            
-	displayTaskArea.innerHTML = ``
+leftArrow.addEventListener('click', ()=>{  
+	$('#divToDoList').empty() 
+	displayTaskArea.innerText = 'Please Pick a Date.'         
+	// displayTaskArea.innerHTML = null
 	dateArr = document.querySelectorAll('.selectable')
 	dateArr.forEach(i => {
 		i.addEventListener('click', (event) => {
@@ -42,8 +45,9 @@ leftArrow.addEventListener('click', ()=>{
 })
 
 rightArrow.addEventListener('click', ()=>{
-	displayTaskArea.innerHTML = ``
-
+	$('#divToDoList').empty() 
+	displayTaskArea.innerText = 'Please Pick a Date.'
+	// displayTaskArea.innerHTML = null
 	dateArr = document.querySelectorAll('.selectable')
 	dateArr.forEach(i => {
 		i.addEventListener('click', (event) => {
@@ -59,57 +63,65 @@ rightArrow.addEventListener('click', ()=>{
 
 // functions
 const getTaskByDate = async (date) => {
-	console.log(date)
-	displayTaskArea.innerHTML = ''
+	$('#divToDoList').empty()
+	// console.log(date)
+	displayTaskArea.innerHTML = '<br>'
 	let response = await fetch('http://localhost:8080/todolist')
 	if (response.ok) {
 		const dataArr = await response.json()
-		console.log(dataArr)
+		// console.log(dataArr)
+		// let arr = []
 		for (let data of dataArr) {
 			if (data.duedate === date) {
-				console.log(date)
+				// arr.push(data)
 				console.log(data.duedate)
 				
 				displayTaskArea.innerHTML +=				
-					`<div class="memo memo-${data.id}" id="${data.id}">
+				`<div class="memo memo-${data.id}" id="${data.id}">
     			<div class="memoTopBorder ${data.type} ${data.type}"></div>
     			<div class="memoTopBar">
-        		<p id="${data.id}" class="memoType blue">Task-${data.id}: ${data.name}</p> 
+        		<p id="${data.id}" class="memoType blue">${data.name}</p> 
         		<svg class="delete-btn" id="${data.id}" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm0 10.293l5.293-5.293.707.707-5.293 5.293 5.293 5.293-.707.707-5.293-5.293-5.293 5.293-.707-.707 5.293-5.293-5.293-5.293.707-.707 5.293 5.293z"/></svg>                           
     			</div>    
     			<div class="memoContent description">${data.description}</div>
-    			<div class="memoContent">${data.type}</div>
-    			<div class="memoContent duedate">Due Date:${data.duedate}</div>
-    			<div class="memoMiniFunction">                     
-        		<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M24 4.685l-16.327 17.315-7.673-9.054.761-.648 6.95 8.203 15.561-16.501.728.685z"/></svg>
+    			<div class="memoMiniFunction">      
         		<svg  class="update-btn" id="${data.id}" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M8.071 21.586l-7.071 1.414 1.414-7.071 14.929-14.929 5.657 5.657-14.929 14.929zm-.493-.921l-4.243-4.243-1.06 5.303 5.303-1.06zm9.765-18.251l-13.3 13.301 4.242 4.242 13.301-13.3-4.243-4.243z"/></svg>
     			</div>    
 				</div>`; 
-				
+				rightArrow.addEventListener('click', ()=>{
+					console.log('del')
+					displayTaskArea.innerText = 'Please Pick a Date.'
+				})
+				leftArrow.addEventListener('click', ()=>{
+					console.log('del')
+					displayTaskArea.innerText = 'Please Pick a Date.'
+				})	
+							
 			}
 		}
+		/*
+		console.log(arr)
+		for(let i = 0; i <arr.length ; i++){
+			displayTaskArea.innerHTML +=				
+			`<div class="memo memo-${arr[i].id}" id="${arr[i].id}">
+			<div class="memoTopBorder ${arr[i].type} ${arr[i].type}"></div>
+			<div class="memoTopBar">
+			<p id="${arr[i].id}" class="memoType blue">${arr[i].name}</p> 
+			<svg class="delete-btn" id="${arr[i].id}" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm0 10.293l5.293-5.293.707.707-5.293 5.293 5.293 5.293-.707.707-5.293-5.293-5.293 5.293-.707-.707 5.293-5.293-5.293-5.293.707-.707 5.293 5.293z"/></svg>                           
+			</div>    
+			<div class="memoContent description">${arr[i].description}</div>
+			<div class="memoMiniFunction">      
+			<svg  class="update-btn" id="${arr[i].id}" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M8.071 21.586l-7.071 1.414 1.414-7.071 14.929-14.929 5.657 5.657-14.929 14.929zm-.493-.921l-4.243-4.243-1.06 5.303 5.303-1.06zm9.765-18.251l-13.3 13.301 4.242 4.242 13.301-13.3-4.243-4.243z"/></svg>
+			</div>    
+			</div>`; 
+			
+		}*/
 	}
+
 }
 
-const showTaskByDate = obj => {
+const clearDisplayArea = (arr) => {
 	divToDoHeader.innerHTML = obj.duedate;
-/*
-	displayTaskArea.innerHTML =
-		`<div class="memo memo-${obj.id}" id="${obj.id}">
-    			<div class="memoTopBorder ${obj.type} ${obj.type}"></div>
-    			<div class="memoTopBar">
-        		<p id="${obj.id}" class="memoType blue">Task-${obj.id}: ${obj.name}</p> 
-        		<svg class="delete-btn" id="${obj.id}" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm0 10.293l5.293-5.293.707.707-5.293 5.293 5.293 5.293-.707.707-5.293-5.293-5.293 5.293-.707-.707 5.293-5.293-5.293-5.293.707-.707 5.293 5.293z"/></svg>                           
-    			</div>    
-    			<div class="memoContent description">${obj.description}</div>
-    			<div class="memoContent">${obj.type}</div>
-    			<div class="memoContent duedate">Due Date:${obj.duedate}</div>
-    			<div class="memoMiniFunction">                     
-        		<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M24 4.685l-16.327 17.315-7.673-9.054.761-.648 6.95 8.203 15.561-16.501.728.685z"/></svg>
-        		<svg  class="update-btn" id="${obj.id}" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M8.071 21.586l-7.071 1.414 1.414-7.071 14.929-14.929 5.657 5.657-14.929 14.929zm-.493-.921l-4.243-4.243-1.06 5.303 5.303-1.06zm9.765-18.251l-13.3 13.301 4.242 4.242 13.301-13.3-4.243-4.243z"/></svg>
-    			</div>    
-				</div>`;
-*/
 
 }
 
